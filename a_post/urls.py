4476ce47fd575730498d9f769c9bfbcdfcf311e8
David Allen <spondicious@me.com>
2024-04-09 22:38:41 +0200

    Blog pages working fine

--- a_post/urls.py
+++ a_post/urls.py
@@ -4,4 +4,5 @@ from a_post import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
+    path('<slug:slug>/', views.post_detail, name='post-detail'),
 ]
--- a_post/views.py
+++ a_post/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def frontpage(request):
     posts = Post.objects.all()
     return render(request, 'a_post/blog_home.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'a_post/post_detail.html', {'post': post})
+
+
