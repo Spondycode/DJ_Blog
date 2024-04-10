c6db9a4b3ee76f5e1d0559f62e6f9d244e26dc7e
David Allen <spondicious@me.com>
2024-04-10 11:47:50 +0200

    Add Comment model and update post_detail.html template

--- a_post/models.py
+++ a_post/models.py
@@ -20,3 +20,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- a_post/views.py
+++ a_post/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+
 
 def frontpage(request):
     posts = Post.objects.all()
@@ -10,4 +11,16 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'a_post/post_detail.html', {'post': post})
 
-
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        comment = request.POST.get('comment')
+        
+        if name and comment:
+            Comment.objects.create(
+                post=post, 
+                name=name, 
+                comment=comment
+                )
+            return redirect('post_detail', slug=slug)
+        
+    return render(request, 'a_post/post_detail.html', {'post': post})
